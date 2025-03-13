a8a511416c2f668aab72a402a88eb2a97c6e4799
JeanPaulM03 <Leksmartinez03@gmail.com>
2025-03-13 00:30:21 -0700

    Update PlayerCharacter.cs

--- PlayerCharacter.cs
+++ PlayerCharacter.cs
@@ -1,21 +1,40 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerCharacter : MonoBehaviour
 {   
-    // Varaible for the Player's health 
     private int health;
+    private int maxHealth = 5;
+    private float regenDelay = 10f;
+    private Coroutine regenCoroutine;
 
-    // Start is called before the first frame update
     void Start()
     {
-        health = 5; // Set Player's health
+        health = maxHealth; 
     }
 
-    // Method to call to deal damage to the player 
-    public void Hurt(int damage) {
+    public void Hurt(int damage) 
+    {
         health -= damage;
         Debug.Log($"Health: {health}");
+        
+        if (regenCoroutine != null)
+        {
+            StopCoroutine(regenCoroutine);
+        }
+        regenCoroutine = StartCoroutine(RegenerateHealth());
+    }
+
+    private IEnumerator RegenerateHealth()
+    {
+        yield return new WaitForSeconds(regenDelay);
+        
+        while (health < maxHealth)
+        {
+            health++;
+            Debug.Log($"Regenerating... Health: {health}");
+            yield return new WaitForSeconds(1f); // Adjust rate of regeneration
+        }
     }
 }
+
